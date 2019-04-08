232a068c9d32581fa7ebbb9190117d327c22eb87
Kévin Casagrande <microscope.kcasagrande@xoxy.net>
2019-04-08 15:38:09 +0200

    Allow for optional parameters.

--- src/Scene/Scene.js
+++ src/Scene/Scene.js
@@ -1,33 +1,49 @@
-const Entity = require('Entity/Entity');
+const Tone = require('Tone/Tone');
 const uuid = require('uuid/v4');
 
 const Scene = (
 	id = uuid()
-) => (
+) => ({
 	question = 'The question',
 	requiredCharacters = [],
 	forbiddenCharacters = [],
 	frame = '',
-	answer = ''
-) => {
+	answer = '',
+	tone = Tone.Light
+}) => {
 	const scene = {
 		id: id,
 		question: question,
 		requiredCharacters: requiredCharacters,
 		forbiddenCharacters: forbiddenCharacters,
 		frame: frame,
-		answer: answer
+		answer: answer,
+		tone: tone
 	};
 
 	const def = require('helper/ImmutableObject').defineMethod(scene);
 
 	def('setQuestion', (question) => {
-		return Scene(id)(question, requiredCharacters, forbiddenCharacters, frame, answer);
+		return Scene(id)({
+			question: question,
+			requiredCharacters: requiredCharacters,
+			forbiddenCharacters: forbiddenCharacters,
+			frame: frame,
+			answer: answer,
+			tone: tone
+		});
 	});
 
 	def('requireCharacter', (requiredCharacter) => {
 		if(requiredCharacters.length < 2) {
-			return Scene(id)(question, requiredCharacters.concat([requiredCharacter]), forbiddenCharacters, frame, answer);
+			return Scene(id)({
+				question: question,
+				requiredCharacters: requiredCharacters.concat([requiredCharacter]),
+				forbiddenCharacters: forbiddenCharacters,
+				frame: frame,
+				answer: answer,
+				tone: tone
+			});
 		}
 		else {
 			throw Error('Can\'t have more than two required characters');
@@ -35,12 +51,26 @@ const Scene = (
 	});
 
 	def('freeCharacter', (freedCharacter) => {
-		return Scene(id)(question, requiredCharacters.filter((character) => character !== freedCharacter), forbiddenCharacters, frame, answer);
+		return Scene(id)({
+			question: question,
+			requiredCharacters: requiredCharacters.filter((character) => character !== freedCharacter),
+			forbiddenCharacters: forbiddenCharacters,
+			frame: frame,
+			answer: answer,
+			tone: tone
+		});
 	});
 
 	def('forbidCharacter', (forbiddenCharacter) => {
 		if(forbiddenCharacters.length < 2) {
-			return Scene(id)(question, requiredCharacters, forbiddenCharacters.concat([forbiddenCharacter]), frame, answer);
+			return Scene(id)({
+				question: question,
+				requiredCharacters: requiredCharacters,
+				forbiddenCharacters: forbiddenCharacters.concat([forbiddenCharacter]),
+				frame: frame,
+				answer: answer,
+				tone: tone
+			});
 		}
 		else {
 			throw Error('Can\'t have more than two forbidden characters');
@@ -48,22 +78,50 @@ const Scene = (
 	});
 
 	def('allowCharacter', (allowedCharacter) => {
-		return Scene(id)(question, requiredCharacters, forbiddenCharacters.filter((character) => character !== allowedCharacter), frame, answer);
+		return Scene(id)({
+			question: question,
+			requiredCharacters: requiredCharacters,
+			forbiddenCharacters: forbiddenCharacters.filter((character) => character !== allowedCharacter),
+			frame: frame,
+			answer: answer,
+			tone: tone
+		});
 	});
 
 	def('setupFrame', (frame) => {
-		return Scene(id)(question, requiredCharacters, forbiddenCharacters, frame, answer);
+		return Scene(id)({
+			question: question,
+			requiredCharacters: requiredCharacters,
+			forbiddenCharacters: forbiddenCharacters,
+			frame: frame,
+			answer: answer,
+			tone: tone
+		});
 	});
 
 	def('setAnswer', (answer) => {
-		return Scene(id)(question, requiredCharacters, forbiddenCharacters, frame, answer);
+		return Scene(id)({
+			question: question,
+			requiredCharacters: requiredCharacters,
+			forbiddenCharacters: forbiddenCharacters,
+			frame: frame,
+			answer: answer,
+			tone: tone
+		});
 	});
 
 	return Object.freeze(scene);
 }
 
 Scene.fromJSON = (json) => {
-	return Scene(json.id)(json.question, json.requiredCharacters.slice());
+	return Scene(json.id)({
+		question: json.question,
+		requiredCharacters: json.requiredCharacters.slice(),
+		forbiddenCharacters: json.forbiddenCharacters.slice(),
+		frame: json.frame,
+		answer: json.answer,
+		tone: Tone.fromJSON(json.tone)
+	});
 }
 
 module.exports = Scene;
--- test/Scene.js
+++ test/Scene.js
@@ -1,15 +1,30 @@
 const should = require('chai').should();
 const assert = require('chai').assert;
 const Scene = require('Scene/Scene');
+const Tone = require('Tone/Tone');
 
 describe('Scene', function() {
 
 	context('setQuestion', function() {
 
 		it('should set the question of the Scene to the given question', function() {
-			const scene = Scene()('Question', ['Required character'], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.setQuestion('New question');
-			result.should.deep.equal(Scene(scene.id)('New question', ['Required character'], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'New question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
@@ -18,24 +33,59 @@ describe('Scene', function() {
 
 		it('should have the required character after requiring a character on a Scene where there was none', function() {
 			const character = 'First character';
-			const scene = Scene()('Question', [], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: [],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.requireCharacter(character);
-			result.should.deep.equal(Scene(scene.id)('Question', [character], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: [character],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should have both required characters after requiring a character on a Scene where there was one', function() {
 			const character1 = 'First character';
 			const character2 = 'Second character';
-			const scene = Scene()('Question', [character1], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: [character1],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.requireCharacter(character2);
-			result.should.deep.equal(Scene(scene.id)('Question', [character1, character2], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: [character1, character2],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should throw when thying to require a character on a Scene where there was already two', function() {
 			const character1 = 'First character';
 			const character2 = 'Second character';
 			const character3 = 'Third character';
-			const scene = Scene()('Question', [character1, character2], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: [character1, character2],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			(() => scene.requireCharacter(character3)).should.throw('Can\'t have more than two required characters');
 		});
 
@@ -44,17 +94,45 @@ describe('Scene', function() {
 	context('freeCharacter', function() {
 
 		it('should remove the freed character from the required characters when it was required on the Scene', function() {
-			const scene = Scene()('Question', ['First required character', 'Second required character'], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['First required character', 'Second required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.freeCharacter('First required character');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Second required character'], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Second required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should leave the required characters when the freed character was not required on the Scene', function() {
 			const character1 = 'First character';
 			const character2 = 'Second character';
-			const scene = Scene()('Question', [character1, character2], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: [character1, character2],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.freeCharacter('Third character');
-			result.should.deep.equal(Scene(scene.id)('Question', [character1, character2], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: [character1, character2],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
@@ -62,19 +140,54 @@ describe('Scene', function() {
 	context('forbidCharacter', function() {
 
 		it('should have the forbidden character when there was none in the Scene', function() {
-			const scene = Scene()('Question', ['Required character'], [], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: [],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.forbidCharacter('Forbidden character');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should have the forbidden character when there was already one in the Scene', function() {
-			const scene = Scene()('Question', ['Required character'], ['First forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['First forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.forbidCharacter('Second forbidden character');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['First forbidden character', 'Second forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['First forbidden character', 'Second forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should throw when there was already two forbidden characters in the Scene', function() {
-			const scene = Scene()('Question', ['Required character'], ['First forbidden character', 'Second forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['First forbidden character', 'Second forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			(() => scene.forbidCharacter('Third forbidden character')).should.throw('Can\'t have more than two forbidden characters');
 		});
 
@@ -83,9 +196,23 @@ describe('Scene', function() {
 	context('allowCharacter', function() {
 
 		it('should remove the allowed character from the forbidden characters when it was forbidden on the Scene', function() {
-			const scene = Scene()('Question', ['Required character'], ['First forbidden character', 'Second forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['First forbidden character', 'Second forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.allowCharacter('First forbidden character');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Second forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Second forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
@@ -93,15 +220,42 @@ describe('Scene', function() {
 	context('setupFrame', function() {
 
 		it('should setup the frame of a Scene when there was none', function() {
-			const scene = Scene()('Question', ['Required character'], ['Forbidden character'], undefined, 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.setupFrame('Frame description');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Forbidden character'], 'Frame description', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame description',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should setup the frame of a Scene when there was already one', function() {
-			const scene = Scene()('Question', ['Required character'], ['Forbidden character'], 'Old frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Old frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.setupFrame('Frame description');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Forbidden character'], 'Frame description', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame description',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
@@ -109,15 +263,42 @@ describe('Scene', function() {
 	context('setAnswer', function() {
 
 		it('should set the answer to the question of a Scene when there was none', function() {
-			const scene = Scene()('Question', ['Required character'], ['Forbidden character'], 'Frame');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				tone: Tone.Light
+			});
 			const result = scene.setAnswer('Answer');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Forbidden character'], 'Frame', 'Answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 		it('should set the answer to the question of a Scene when there was already one', function() {
-			const scene = Scene()('Question', ['Required character'], ['Forbidden character'], 'Frame', 'Answer');
+			const scene = Scene()({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			});
 			const result = scene.setAnswer('New answer');
-			result.should.deep.equal(Scene(scene.id)('Question', ['Required character'], ['Forbidden character'], 'Frame', 'New answer'));
+			result.should.deep.equal(Scene(scene.id)({
+				question: 'Question',
+				requiredCharacters: ['Required character'],
+				forbiddenCharacters: ['Forbidden character'],
+				frame: 'Frame',
+				answer: 'New answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
@@ -127,10 +308,21 @@ describe('Scene', function() {
 		it('should format to a correct JSON', function() {
 			const scene = Scene.fromJSON({
 				id: 'id',
-				question: 'What?',
-				requiredCharacters: ['Character 1']
+				question: 'Question',
+				requiredCharacters: ['Character 1', 'Character 2'],
+				forbiddenCharacters: ['Character 3', 'Character 4'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: 'Light'
 			});
-			scene.should.deep.equal(Scene('id')('What?', ['Character 1']));
+			scene.should.deep.equal(Scene('id')({
+				question: 'Question',
+				requiredCharacters: ['Character 1', 'Character 2'],
+				forbiddenCharacters: ['Character 3', 'Character 4'],
+				frame: 'Frame',
+				answer: 'Answer',
+				tone: Tone.Light
+			}));
 		});
 
 	});
